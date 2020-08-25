be55fc76e3092a129542869c9e4509975b62d4e7
Everson Pereira <everson@everson.net.br>
2020-08-25 14:23:59 -0300

    Adicionado Dockerfile, criado arquivos yaml para criar deployment e service no k8s, corrigido link do star wars api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const axios = require('axios');
-const baseURL = 'https://swapi.co/api/';
+const baseURL = 'https://swapi.dev/api/';
 
 const getFilmId = (url) => {
     const id = url.split('/')[5];
